fcf573100e9d91625fa5594f13ae91029264dedc
TrazZed <hayestristan26@gmail.com>
2024-03-29 01:58:59 +1000

    Guild Connection and Member Joining Messages

--- bot.py
+++ bot.py
@@ -7,12 +7,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+intents.presences = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord')
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    print(f'{client.user} has connected to the following guild:\n'f'{guild.name}(id: {guild.id})')
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hello {member.name}! Welcome to TrazZed Sends Not Private Stuff'
+    )
 
 client.run(TOKEN)
 
